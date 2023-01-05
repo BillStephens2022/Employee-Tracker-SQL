1b1b76fde640cfbe87c042cb54d7f54cbf23bc54
BillStephens2022 <two4onebill@yahoo.com>
2023-01-05 15:51:12 -0500

    added functions for displaying tables for departments, roles, employees

---
+++ db/query.sql
@@ -0,0 +1,17 @@
+
+-- view all departments
+SELECT * FROM department;
+
+-- view all roles
+SELECT role.id, title, department.name AS department, salary 
+FROM role
+INNER JOIN department ON role.department_id = department.id;
+
+-- view all employees
+SELECT employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, 
+CONCAT(e.first_name, ' ',e.last_name) AS manager
+FROM employee
+INNER JOIN role ON role.id = employee.role_id
+INNER JOIN department ON department.id = role.department_id
+LEFT JOIN employee e ON employee.manager_id = e.id;
+
--- server.js
+++ server.js
@@ -21,6 +21,39 @@ const db = mysql.createConnection(
     console.log('Connected to the employees_db database')
 );
 
+// function to run sql query to view all employees
+function viewAllEmployees() {
+    db.query(`SELECT employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, 
+    CONCAT(e.first_name, ' ',e.last_name) AS manager
+    FROM employee
+    INNER JOIN role ON role.id = employee.role_id
+    INNER JOIN department ON department.id = role.department_id
+    LEFT JOIN employee e ON employee.manager_id = e.id;`, (err, results) => {
+        (err) ? console.log(err) : console.table(results);
+    });
+}
+
+// function to run an sql query to view all employees
+function viewAllDepartments() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        (err) ? console.log(err) : console.table(results);
+    });
+}
+
+// function to run an sql query to view all employees
+function viewAllRoles() {
+    db.query(`SELECT role.id, title, department.name AS department, salary 
+    FROM role INNER JOIN department ON role.department_id = department.id;`, (err, results) => {
+        (err) ? console.log(err) : console.table(results);
+    });
+}
+
+viewAllDepartments();
+viewAllRoles();
+viewAllEmployees();
+
+
+
 // set up Express server to listen on PORT defined above.
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
