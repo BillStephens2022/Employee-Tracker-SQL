e09dc8246ae374826aa57e37e702a70247e4412e
BillStephens2022 <two4onebill@yahoo.com>
2023-01-05 07:46:47 -0500

    initial commit, set up express server, installed npm packages

---
+++ server.js
@@ -0,0 +1,22 @@
+// Import & Require
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Database connection
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Password123!',
+        database: 'employees_db'
+    },
+    console.log('Connected to the employees_db database')
+);
